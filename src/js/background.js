b4e47a6c25d6d32891b52b3a137dd754b5981a39
Alexei <alexeiatyahoodotcom@gmail.com>
2014-05-14 10:42:37 -0400

    Revert Accept header update for now.
    Needs more investigation.

--- src/js/background.js
+++ src/js/background.js
@@ -53,7 +53,9 @@ var tabData = (function () {
 
 var HEADER_OVERRIDES = {
 	'User-Agent': "Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0",
-	'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+	// TODO this matches Tor Browser on http://fingerprint.pet-portal.eu/?lang=en but not on Panopticlick ...
+	//'Accept': "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+	'Accept': "text/html, */*",
 	'Accept-Language': "en-us,en;q=0.5",
 	'Accept-Encoding': "gzip, deflate",
 	'DNT': null // remove to match Tor Browser
