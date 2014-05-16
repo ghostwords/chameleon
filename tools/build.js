3dc58f8f6f9cbf834acf975d5f4fc1500ca4b64c
Alexei <alexeiatyahoodotcom@gmail.com>
2014-05-15 20:18:16 -0400

    Add license header to build scripts.

--- tools/build.js
+++ tools/build.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 
+/*!
+ * Chameleon
+ *
+ * Copyright 2014 ghostwords.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 var fs = require('fs'),
 	glob = require('glob'),
 	path = require('path');
--- tools/dist.js
+++ tools/dist.js
@@ -1,5 +1,16 @@
 #!/usr/bin/env node
 
+/*!
+ * Chameleon
+ *
+ * Copyright 2014 ghostwords.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
 var moment = require('moment'),
 	shell = require('shelljs');
 
