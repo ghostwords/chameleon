ghostwords/chameleon

commit fbb031ad7deb823aacea94525943078777c97b0a

Refactor tabData into a module.

chrome/js/builds/background.js
@@ -19,40 +19,7 @@ var _ = require('underscore');
 var ALL_URLS = { urls: ['http://*/*', 'https://*/*'] },
 	ENABLED = true;
 
-var tabData = (function () {
-	var data = {};
-
-	return {
-		record: function (tab_id, access) {
-			if (!data.hasOwnProperty(tab_id)) {
-				data[tab_id] = {
-					accesses: []
-				};
-			}
-			if (access.prop == 'style.fontFamily') {
-				data[tab_id].fontEnumeration = true;
-			} else {
-				data[tab_id].accesses.push(access);
-			}
-		},
-		get: function (tab_id) {
-			return data.hasOwnProperty(tab_id) && data[tab_id];
-		},
-		clear: function (tab_id) {
-			delete data[tab_id];
-		},
-		clean: function () {
-			chrome.tabs.query({}, function (tabs) {
-				// get tab IDs that are in "data" but no longer a known tab
-				// and clean up orphan data
-				_.difference(
-					Object.keys(data).map(Number),
-					_.pluck(tabs, 'id')
-				).forEach(tabData.clear);
-			});
-		}
-	};
-}());
+var tabData = require('../lib/tabdata');
 
 var HEADER_OVERRIDES = {
 	'User-Agent': "Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0",
@@ -233,5 +200,50 @@ chrome.webNavigation.onCommitted.addListener(onNavigation);
 // see if we have any orphan data every five minutes
 // TODO switch to chrome.alarms?
 setInterval(tabData.clean, 300000);
+
+},{"../lib/tabdata":3}],3:[function(require,module,exports){
+/*!
+ * Chameleon
+ *
+ * Copyright 2014 ghostwords.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+var _ = require('underscore');
+
+var data = {};
+
+var tabData = {
+	record: function (tab_id, access) {
+		if (!data.hasOwnProperty(tab_id)) {
+			data[tab_id] = {
+				accesses: []
+			};
+		}
+		data[tab_id].accesses.push(access);
+	},
+	get: function (tab_id) {
+		return data.hasOwnProperty(tab_id) && data[tab_id];
+	},
+	clear: function (tab_id) {
+		delete data[tab_id];
+	},
+	clean: function () {
+		chrome.tabs.query({}, function (tabs) {
+			// get tab IDs that are in "data" but no longer a known tab
+			// and clean up orphan data
+			_.difference(
+				Object.keys(data).map(Number),
+				_.pluck(tabs, 'id')
+			).forEach(tabData.clear);
+		});
+	}
+};
+
+module.exports = tabData;
 
 },{}]},{},[2])

src/lib/tabdata.js
@@ -1,0 +1,43 @@
+/*!
+ * Chameleon
+ *
+ * Copyright 2014 ghostwords.
+ *
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/.
+ *
+ */
+
+var _ = require('underscore');
+
+var data = {};
+
+var tabData = {
+	record: function (tab_id, access) {
+		if (!data.hasOwnProperty(tab_id)) {
+			data[tab_id] = {
+				accesses: []
+			};
+		}
+		data[tab_id].accesses.push(access);
+	},
+	get: function (tab_id) {
+		return data.hasOwnProperty(tab_id) && data[tab_id];
+	},
+	clear: function (tab_id) {
+		delete data[tab_id];
+	},
+	clean: function () {
+		chrome.tabs.query({}, function (tabs) {
+			// get tab IDs that are in "data" but no longer a known tab
+			// and clean up orphan data
+			_.difference(
+				Object.keys(data).map(Number),
+				_.pluck(tabs, 'id')
+			).forEach(tabData.clear);
+		});
+	}
+};
+
+module.exports = tabData;

src/js/background.js
@@ -16,40 +16,7 @@ var _ = require('underscore');
 var ALL_URLS = { urls: ['http://*/*', 'https://*/*'] },
 	ENABLED = true;
 
-var tabData = (function () {
-	var data = {};
-
-	return {
-		record: function (tab_id, access) {
-			if (!data.hasOwnProperty(tab_id)) {
-				data[tab_id] = {
-					accesses: []
-				};
-			}
-			if (access.prop == 'style.fontFamily') {
-				data[tab_id].fontEnumeration = true;
-			} else {
-				data[tab_id].accesses.push(access);
-			}
-		},
-		get: function (tab_id) {
-			return data.hasOwnProperty(tab_id) && data[tab_id];
-		},
-		clear: function (tab_id) {
-			delete data[tab_id];
-		},
-		clean: function () {
-			chrome.tabs.query({}, function (tabs) {
-				// get tab IDs that are in "data" but no longer a known tab
-				// and clean up orphan data
-				_.difference(
-					Object.keys(data).map(Number),
-					_.pluck(tabs, 'id')
-				).forEach(tabData.clear);
-			});
-		}
-	};
-}());
+var tabData = require('../lib/tabdata');
 
 var HEADER_OVERRIDES = {
 	'User-Agent': "Mozilla/5.0 (Windows NT 6.1; rv:24.0) Gecko/20100101 Firefox/24.0",

commit 0ee64b3ebcdee0301122b80f21292596297b2525

Fix font enumeration detection flag.
Broken by fbb031a.

chrome/js/builds/background.js
@@ -224,7 +224,11 @@ var tabData = {
 				accesses: []
 			};
 		}
-		data[tab_id].accesses.push(access);
+		if (access.prop == 'style.fontFamily') {
+			data[tab_id].fontEnumeration = true;
+		} else {
+			data[tab_id].accesses.push(access);
+		}
 	},
 	get: function (tab_id) {
 		return data.hasOwnProperty(tab_id) && data[tab_id];

src/lib/tabdata.js
@@ -20,7 +20,11 @@ var tabData = {
 				accesses: []
 			};
 		}
-		data[tab_id].accesses.push(access);
+		if (access.prop == 'style.fontFamily') {
+			data[tab_id].fontEnumeration = true;
+		} else {
+			data[tab_id].accesses.push(access);
+		}
 	},
 	get: function (tab_id) {
 		return data.hasOwnProperty(tab_id) && data[tab_id];