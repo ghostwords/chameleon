d4102ccf676e9dc7b6576b910384f39b5ec549f7
Alexei <alexeiatyahoodotcom@gmail.com>
2014-07-18 15:58:36 -0400

    Add TODOs.

--- tools/dist.js
+++ tools/dist.js
@@ -23,7 +23,16 @@ var package_name = [
 // copy chrome/** to dist/
 shell.cp('-R', 'chrome/**', 'dist/' + package_name);
 
-// create the package
+// create the drag-and-drop installable CRX package
 shell.exit(shell.exec(
 	'cd dist && ../tools/crxmake.sh ' + package_name + ' ~/.ssh/chameleon.pem'
 ).code);
+
+// TODO create the zip for uploading to Chrome Web Store
+// TODO make this is the "release" task (also tags, ...)
+// https://developer.chrome.com/extensions/packaging
+// 1. Rename the private key that was generated when you created the .crx file to key.pem.
+// 2. Put key.pem in the top directory of your extension.
+// 3. Compress that directory into a ZIP file.
+// 4. Upload the ZIP file using the Chrome Developer Dashboard.
+// TODO No need to do the key.pem thing for updates, right?
